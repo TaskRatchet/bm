42f32fa1f5fc9af67576f3c91691601cac52502b
Nathan Arthur <nathan@nathanarthur.com>
2023-02-07 12:16:43 -0500

    refactor bm wrapper

--- src/bm.ts
+++ src/bm.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 
+const API_ROOT = "https://www.beeminder.com/api/v1";
+
 export async function getGoals(token: string) {
-  const url = `https://www.beeminder.com/api/v1/users/me/goals.json?access_token=${token}`;
+  const url = `${API_ROOT}/users/me/goals.json?access_token=${token}`;
   const response = await axios.get(url);
 
   return response.data;
 }
 
 export async function refreshGraph(token: string, goal: string) {
-  const url = `https://www.beeminder.com/api/v1/users/me/goals/${goal}/refresh_graph.json?access_token=${token}`;
+  const url = `${API_ROOT}/users/me/goals/${goal}/refresh_graph.json?access_token=${token}`;
   const response = await axios.get(url);
 
   return response.data;
@@ -19,7 +21,7 @@ export async function createDatapoint(
   goal: string,
   value: number
 ) {
-  const url = `https://www.beeminder.com/api/v1/users/me/goals/${goal}/datapoints.json?access_token=${token}`;
+  const url = `${API_ROOT}/users/me/goals/${goal}/datapoints.json?access_token=${token}`;
   const response = await axios.post(url, {
     value,
   });
