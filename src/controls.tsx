02a31d8f26e6f1c423dc54411bdf5ca195485b89
Nathan Arthur <nathan@nathanarthur.com>
2023-02-08 06:51:35 -0500

    attempt windows emoji fix

--- src/controls.tsx
+++ src/controls.tsx
@@ -31,7 +31,7 @@ export default function Controls({
         class={`icon-button ${isRefreshing && "spin"}`}
         onClick={() => refresh()}
       >
-        🔄
+        🔃
       </button>
     );
   }
