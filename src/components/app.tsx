6f5380f3d0f7a680b60202c8535c41c467a25984
Nathan Arthur <nathan@nathanarthur.com>
2023-02-18 08:53:32 -0500

    sticky footer

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -23,78 +23,79 @@ function _App() {
 
   return (
     <>
-      <Colors goals={data} />
+      <div>
+        <Colors goals={data} />
 
-      <Time goals={data} />
+        <Time goals={data} />
 
-      <div class="global-controls">
-        <span class="filter">
-          <input
-            type="text"
-            placeholder="filter"
-            value={search}
-            onChange={(e: any) => setSearch(e.target.value)}
-          />
-          <button class="icon-button" onClick={() => setSearch("")}>
-            ❌
-          </button>
-        </span>
+        <div class="global-controls">
+          <span class="filter">
+            <input
+              type="text"
+              placeholder="filter"
+              value={search}
+              onChange={(e: any) => setSearch(e.target.value)}
+            />
+            <button class="icon-button" onClick={() => setSearch("")}>
+              ❌
+            </button>
+          </span>
 
-        <span class="buttons">
-          <a
-            class="icon-button"
-            href="https://beeminder.com/new"
-            title="Add goal"
-          >
-            ➕
-          </a>
-          <button
-            class="icon-button"
-            onClick={() => {
-              const start = window.prompt("Start date (YYYY-MM-DD)");
-              const finish = window.prompt("Finish date (YYYY-MM-DD)");
-              const url = `https://beeminder.com/breaks?start=${start}&finish=${finish}`;
-              window.open(url);
-            }}
-            title="Add breaks"
-          >
-            🏖️
-          </button>
-          <a
-            class="icon-button"
-            href="https://beeminder.com/settings/account"
-            title="Account settings"
-          >
-            ⚙️
-          </a>
-          <a
-            class="icon-button"
-            href="https://help.beeminder.com/"
-            title="Docs"
-          >
-            ❓
-          </a>
-          <a
-            class="icon-button"
-            href="https://www.beeminder.com/premium"
-            title="Premium"
-          >
-            💎
-          </a>
-          <button class="icon-button" onClick={logout} title="Logout">
-            🚪
-          </button>
-          <button
-            class={`icon-button ${isFetching && "spin"}`}
-            onClick={() => refetch()}
-            title="Refresh"
-          >
-            🔃
-          </button>
-        </span>
+          <span class="buttons">
+            <a
+              class="icon-button"
+              href="https://beeminder.com/new"
+              title="Add goal"
+            >
+              ➕
+            </a>
+            <button
+              class="icon-button"
+              onClick={() => {
+                const start = window.prompt("Start date (YYYY-MM-DD)");
+                const finish = window.prompt("Finish date (YYYY-MM-DD)");
+                const url = `https://beeminder.com/breaks?start=${start}&finish=${finish}`;
+                window.open(url);
+              }}
+              title="Add breaks"
+            >
+              🏖️
+            </button>
+            <a
+              class="icon-button"
+              href="https://beeminder.com/settings/account"
+              title="Account settings"
+            >
+              ⚙️
+            </a>
+            <a
+              class="icon-button"
+              href="https://help.beeminder.com/"
+              title="Docs"
+            >
+              ❓
+            </a>
+            <a
+              class="icon-button"
+              href="https://www.beeminder.com/premium"
+              title="Premium"
+            >
+              💎
+            </a>
+            <button class="icon-button" onClick={logout} title="Logout">
+              🚪
+            </button>
+            <button
+              class={`icon-button ${isFetching && "spin"}`}
+              onClick={() => refetch()}
+              title="Refresh"
+            >
+              🔃
+            </button>
+          </span>
+        </div>
+        <Goals goals={filtered} />
       </div>
-
-      <Goals goals={filtered} />
 
       <small class="footer">
         <span>
