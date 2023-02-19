ad09b42e29cd241a00895787c081bee83cf1b1fc
Nathan Arthur <nathan@nathanarthur.com>
2023-02-19 09:47:14 -0500

    terse

--- src/components/goals.tsx
+++ src/components/goals.tsx
@@ -1,26 +1,28 @@
 import { Goal } from "../bm";
-import GoalComponent from "./goal";
+import G from "./goal";
 import "./goals.css";
 
 export function Goals({ goals = [] }: { goals: Goal[] }) {
   goals.sort((a, b) => (a.losedate < b.losedate ? -1 : 1));
-  const today = goals.filter((g: Goal) => g.safebuf === 0);
-  const next = goals.filter((g: Goal) => g.safebuf !== 0 && !g.todayta);
-  const later = goals.filter((g: Goal) => g.safebuf !== 0 && g.todayta);
+  const t = goals.filter((g: Goal) => g.safebuf === 0);
+  const n = goals.filter((g: Goal) => g.safebuf !== 0 && !g.todayta);
+  const l = goals.filter((g: Goal) => g.safebuf !== 0 && g.todayta);
 
   return (
     <div class="goals">
       <h1>Today</h1>
-      {today.map((g: Goal) => (
-        <GoalComponent key={g.slug} g={g} />
+      {t.map((g: Goal) => (
+        <G key={g.slug} g={g} />
       ))}
+
       <h1>Next</h1>
-      {next.map((g: Goal) => (
-        <GoalComponent key={g.slug} g={g} />
+      {n.map((g: Goal) => (
+        <G key={g.slug} g={g} />
       ))}
+
       <h1>Later</h1>
-      {later.map((g: Goal) => (
-        <GoalComponent key={g.slug} g={g} />
+      {l.map((g: Goal) => (
+        <G key={g.slug} g={g} />
       ))}
     </div>
   );
