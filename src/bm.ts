7a756cfab328cfb3ab852b186c42b5fb4e9587c8
Nathan Arthur <nathan@nathanarthur.com>
2023-02-26 10:45:44 -0500

    fix bm api wrapper

--- src/bm.ts
+++ src/bm.ts
@@ -141,9 +141,16 @@ async function api({
   method?: "get" | "post" | "put" | "delete";
   data?: Record<string, unknown>;
 }) {
+  const form = new URLSearchParams();
+  if (data) {
+    Object.entries(data).forEach(([key, value]) => {
+      form.append(key, value as string);
+    });
+  }
+
   const result = await fetch(`${API_ROOT}${route}?auth_token=${API_KEY}`, {
     method,
-    body: JSON.stringify(data),
+    body: data && form,
   });
 
   if (!result.ok) {
