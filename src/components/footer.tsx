2759f38de55b3631d8ef52541aed526c25b7f6d8
Nathan Arthur <nathan@nathanarthur.com>
2023-02-26 18:41:04 -0500

    refactor footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,26 +1,26 @@
+import { ComponentChildren } from "preact";
 import "./footer.css";
+
+function Link({
+  href,
+  children,
+}: {
+  href: string;
+  children: ComponentChildren;
+}) {
+  return (
+    <a href={href} target="_blank" rel="noreferrer">
+      {children}
+    </a>
+  );
+}
 
 export default function Footer() {
   return (
     <small class="footer">
-      <span>
-        Made by{" "}
-        <a href="https://nathanarthur.com/" target="_blank" rel="noreferrer">
-          Narthur
-        </a>
-        .{" "}
-        <a
-          href="https://github.com/TaskRatchet/bm"
-          target="_blank"
-          rel="noreferrer"
-        >
-          View source
-        </a>
-        . See also:{" "}
-        <a href="https://taskratchet.com" target="_blank" rel="noreferrer">
-          TaskRatchet
-        </a>
-      </span>
+      Made by <Link href="https://nathanarthur.com/">Narthur</Link>.{" "}
+      <Link href="https://github.com/TaskRatchet/bm">View source</Link>. See
+      also: <Link href="https://taskratchet.com">TaskRatchet</Link>
     </small>
   );
 }
