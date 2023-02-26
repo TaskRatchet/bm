13cf8fa4d0e061e65b58fe38cb7b20eeff9ab501
Nathan Arthur <nathan@nathanarthur.com>
2023-02-26 18:30:34 -0500

    terse useGoals

--- src/useGoals.ts
+++ src/useGoals.ts
@@ -1,21 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { API_KEY, logout } from "./auth";
 import { getGoals, Goal } from "./bm";
-import queryClient from "./queryClient";
 
 export default function useGoals() {
   return useQuery<Goal[], Response>(["goals"], () => getGoals(), {
     enabled: !!API_KEY,
-    refetchInterval: () => {
-      try {
-        const goals = queryClient.getQueryData<Goal[]>(["goals"]);
-        const queued = goals?.find((goal) => goal.queued);
-        return queued ? 3000 : 60000;
-      } catch (e) {
-        console.error(e);
-        return 60000;
-      }
-    },
+    refetchInterval: (goals) => (goals?.find((g) => g.queued) ? 3000 : 60000),
     refetchIntervalInBackground: false,
     retry: false,
     onError: (err: Response) => {
